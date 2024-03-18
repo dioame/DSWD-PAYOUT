cd2df15e59680715ebfa6ac009d208d4446e5d2c
baynte <jolan.owang@gmail.com>
2024-03-18 14:00:28 +0800

    Generate PDF - (Backend)

--- app/Services/Admin/Capture/GetCaptureService.php
+++ app/Services/Admin/Capture/GetCaptureService.php
@@ -4,23 +4,29 @@ namespace App\Services\Admin\Capture;
 
 use App\Services\BaseService;
 use App\Models\Admin\Capture;
+use App\Models\Admin\Payroll;
+use Illuminate\Support\Facades\DB;
 
 class GetCaptureService extends BaseService
 {
     public function execute($params = null)
     {
-        $sql = "";
         if($params['range']){
             $range = explode("-",$params['range']);
             $range_from = trim($range[0]);
             $range_to = isset($range[1]) ? trim($range[1]) : trim($range[0]);
             $range_to = empty($range_to) ? $range_from : $range_to;
             $range_from = empty($range_from) ? $range_to : $range_from;
-
-            $sql = "payroll_no BETWEEN $range_from AND $range_to AND deleted_at IS NULL";
         }
-        
-        $capture =  Capture::orderByRaw("CAST(payroll_no AS UNSIGNED) ASC")->whereRaw($sql)->get();
+        $total = $range_to - $range_from + 1;
+            
+        $capture = Payroll::leftJoinSub("SELECT * FROM capture WHERE ISNULL(deleted_at)", "c", "payroll.payroll_no", "=", "c.payroll_no")
+            ->orderByRaw("CAST(payroll.payroll_no as UNSIGNED) ASC")
+            ->skip($range_from - 1)
+            ->take($total)
+            ->select('payroll.payroll_no', 'payroll.name', 'c.path', 'payroll.barangay', 'payroll.municipality', 'c.captured_by', 'c.captured_at')
+            ->get();
+    
         return $capture;   
     }
 }
--- resources/views/print/pdf-template.blade.php
+++ resources/views/print/pdf-template.blade.php
@@ -1,40 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pictures PDF</title>
 </head>
+
 <body>
-<center>
-    
-    @foreach($chunks as $chunk)
-    <span>DSWD Caraga <br>Photo Documentation</span>
+    <center>
+        @foreach($chunks as $chunk)
+        <span>DSWD Caraga <br>Photo Documentation</span>
         <table style='width:100%;'>
-      
+
             @foreach($chunk as $picture)
-                <tr>
-                    @foreach($picture as $row)
-                    <?php 
-                     $path  = asset('storage/pictures/' . basename($row['path']));
+            <tr>
+                @foreach($picture as $row)
+                <?php
+                $path = asset('storage/assets/unclaimed.png');
+                if ($row['path']) {
+                    $path  = asset('storage/pictures/' . basename($row['path']));
+                }
 
-                     
-                    ?>
-                        <td>
-                            <div style='width:300px; height:350px; border:1px solid black; margin:auto; margin-top:50px; position:relative;'>
-                                <img src="{{ $path }}" alt="Image" style='width:100%; height:100%; object-fit: cover;'>
-
-                                <p style='position:absolute;bottom:0;right:10;font-size:8px;font-weight:bold;font-family:arial;color:yellow;background-color: rgba(0, 0, 0, 0.3);padding:3px;border-radius:5px;'>Payroll #<?= $row['payroll_no'] ?> - <?= date('M d, Y H:iA',strtotime($row['captured_at'])) ?></p>
-                            </div>
-                        </td>
-                    @endforeach
-                </tr>
+                ?>
+                <td>
+                    <div style='width:300px; height:350px; border:1px solid black; margin:auto; margin-top:50px; position:relative;'>
+                        <img src="{{ $path }}" alt="Image" style='width:100%; height:100%; object-fit: cover;'>
+                        @if($row['path'])
+                        <p style='position:absolute;bottom:0;right:10;font-size:8px;font-weight:bold;font-family:arial;color:yellow;background-color: rgba(0, 0, 0, 0.3);padding:3px;border-radius:5px;'>Payroll #<?= $row['payroll_no'] ?> - <?= date('M d, Y H:iA', strtotime($row['captured_at'])) ?></p>
+                        @else
+                        <p style="text-align: center; position:absolute;bottom:0;right:0;">Payroll #<?= $row['payroll_no'] ?> - <?= $row['name']?></p>
+                        @endif
+                    </div>
+                </td>
+                @endforeach
+            </tr>
             @endforeach
         </table>
         <div style="page-break-after: always;"></div>
-    @endforeach
-</center>
-
+        @endforeach
+    </center>
 
 </body>
+
 </html>
