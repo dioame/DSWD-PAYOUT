4c1cdcb773784e88ec9d1c2851cb33f139abd26f
dioamejade@gmail.com <dioamejade@gmail.com>
2024-08-01 11:31:49 +0800

    Update CaptureController.php

--- app/Http/Controllers/Web/Admin/Capture/CaptureController.php
+++ app/Http/Controllers/Web/Admin/Capture/CaptureController.php
@@ -138,16 +138,22 @@ class CaptureController extends Controller
         foreach ($folder as $file) {
             $filename = $file->getClientOriginalName();
             $file->storeAs('public/pictures/'.$fileDirectoryName, $filename);
-            $filePath = "pictures/".$fileDirectoryName."/".$filename;
-
+    
+            // Get the full path to the file
+            $filePath = storage_path('app/public/pictures/'.$fileDirectoryName.'/'.$filename);
+    
+            // Set the permissions to be readable
             chmod($filePath, 0644);
+    
+            // Change the owner of the file to 'kalahi'
             chown($filePath, 'kalahi');
-
-            $payrollNo = explode("_",$filename)[0];
-
+    
+            $publicFilePath = "pictures/".$fileDirectoryName."/".$filename;
+            $payrollNo = explode("_", $filename)[0];
+    
             $service->execute([
                 'payroll_no' => $payrollNo,
-                'path' => $filePath,
+                'path' => $publicFilePath,
                 'id_number' => $idNumber,
                 'municipality' => $municipality,
                 'modality' => $modality,
