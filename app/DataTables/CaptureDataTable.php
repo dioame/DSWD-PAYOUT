cc06f3cee3eccdd8a6d0e84ce2a318b64d45b1c4
dioamejade@gmail.com <dioamejade@gmail.com>
2024-08-05 09:42:45 +0800

    Update CaptureDataTable.php

--- app/DataTables/CaptureDataTable.php
+++ app/DataTables/CaptureDataTable.php
@@ -60,14 +60,14 @@ class CaptureDataTable extends DataTable
     public function query(Capture $model): QueryBuilder
     {
         return $model->newQuery()
-        ->select('capture.*')
-        ->join('payroll', function ($join) {
-            $join->on('capture.payroll_no', '=', 'payroll.payroll_no')
-                 ->on('capture.municipality', '=', 'payroll.municipality')
-                 ->on('capture.modality', '=', 'payroll.modality')
-                 ->on('capture.year', '=', 'payroll.year');
-        })
-        ->orderByDesc('capture.created_at');
+            ->select('capture.*')
+            ->join('payroll', function ($join) {
+                $join->on('capture.payroll_no', '=', 'payroll.payroll_no')
+                     ->where('capture.municipality', '=', 'payroll.municipality')
+                     ->where('capture.modality', '=', 'payroll.modality')
+                     ->where('capture.year', '=', 'payroll.year');
+            })
+            ->orderByDesc('capture.created_at');
     }
 
     public function countRecords(): int
